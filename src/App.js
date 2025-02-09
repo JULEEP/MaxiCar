f64710d6577089ff5381e310e9ce624a8c0c0c7b
Delta Student <sabreenkoushar@gmail.com>
2025-02-10 00:19:36 +0530

    t&c

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
+
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import DemoPage from "./Pages/DemoPage";
 import HomePage from './Pages/Homepage'
 import PricingCart from "./Pages/Pricing";
+import TermCondition from "./Pages/TermCondition";
+
 
 function App() {
   return (
+    <>
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/demo" element={<DemoPage />} />
         <Route path="/price" element={<PricingCart />} />
+        <Route path="/term" element={<TermCondition />} />
+
 
       </Routes>
     </Router>
+    </>
   );
 }
 
